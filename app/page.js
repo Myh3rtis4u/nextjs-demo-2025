b5575cb21b96c82ea59ec4ff0be2cb4f91dd0ab2
Myh3rtis4u <gusjangza11@gmail.com>
2025-11-09 16:04:19 +0700

    Product

--- app/page.js
+++ app/page.js
@@ -1,18 +1,74 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import FormComponent from "@/components/FormComponent";
+import { useRouter } from "next/navigation";
+// import image1 from "../public/assets/images/image1.png";
 
 export default function Home() {
+  const router = useRouter();
+  const [products, setProduct] = useState([]);
+  const [filtersProduct, setFilteredProduct] = useState([]);
+
+  // function
+  const getAllProduct = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/products");
+      //console.log("-getAllProduct-", response);
+
+      if (!response.ok) throw new Error("Failed");
+
+      const allProduct = await response.json();
+      //console.log("-allProduct-", allProduct.products, allProduct.limit);
+      setProduct(allProduct.products);
+      setFilteredProduct(allProduct.products);
+    } catch (error) {
+      setProduct([]);
+      setFilteredProduct([]);
+      //console.log("error", error);
+    }
+  };
+  console.log("-products-", products);
+  console.log("-filteredproducts-", filtersProduct);
+
+  // ใช้ในกรณที่ให้เข้า getAllProduct ครั้งเดียว
+  useEffect(() => {
+    getAllProduct();
+  }, []);
+
+  const handleSearch = (text) => {
+    const filterproducts = products.filter((p) =>
+      p.title.toLowerCase().includes(text.toLowerCase())
+    );
+
+    setFilteredProduct(filterproducts);
+  };
+
   return (
     <div>
       <Header />
-      <FormComponent />
-
+      <FormComponent onSearch={handleSearch} />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 m-10">
-        {/* Code */}
+        {filtersProduct.map((item) => (
+          <div
+            key={item.id}
+            className="p-3 bg-slate-400 flex flex-col items-center border border-gray-500 rounded-2xl hover:shadow-lg cursor-pointer"
+            onClick={() => router.push(`/products/${item.id}`)}
+          >
+            <img alt={item.title} src={item.thumbnail}></img>
+            <p className="text-[16px] font-bold text-center">{item.title}</p>
+            <p className="text-[16px]"> {item.price}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
+}
+// บรรทัด 13 grid ทำให้แบ่งช่องได้ง่ายขึ้น
+// .map เหมือน for loop แต่สั้นกว่า loop เอาพวกรูปสินค้าออกมา
+// useRouter ใช้ในการย้ายหน้า
+// .push('') เปลี่ยน หน้า
+{
+  /* <img alt={"image"} src={image1.src}></img> */
 }
--- components/FormComponent.js
+++ components/FormComponent.js
@@ -1,16 +1,29 @@
 import { Button, Input, InputGroup } from "reactstrap";
 import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
 
-export default function FormComponent() {
+export default function FormComponent({ onSearch }) {
+  const [textSearch, setTextSearch] = useState("");
+  console.log("-textSearch-", textSearch);
   return (
     <div className="bg-blue-50 py-6 px-4">
-      <form className="flex flex-col sm:flex-row justify-center items-center gap-3">
+      <form
+        className="flex flex-col sm:flex-row justify-center items-center gap-3"
+        onSubmit={(event) => {
+          event.preventDefault();
+          onSearch(textSearch);
+        }}
+      >
         <InputGroup className="max-w-md">
-          <Input placeholder="Search products" />
+          <Input
+            placeholder="Search products"
+            value={textSearch}
+            onChange={(event) => setTextSearch(event.target.value)}
+          />
         </InputGroup>
 
         <Button
-          color="primary"
+          color="success"
           size="sm"
           type="submit"
           className="flex! items-center gap-1"
@@ -22,3 +35,8 @@ export default function FormComponent() {
     </div>
   );
 }
+// py คือ padding top, bottom รวมกัน
+// flex-col
+// sm ขนาด เรสป้อนซีฟของแท้บเล้ต ไอแพด
+// max-w-md จัดหน้าจอ ไม่ให้ยาวเกิน
+// event.preventDefault freeze หน้า ไม่ให้ขึ้นหน้า load
